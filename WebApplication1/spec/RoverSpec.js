49a34f0f7d6ae1cae55a537f3ebbc8eaccbb6f3a
anthonyjba <anthonyjba@gmail.com>
2013-09-30 18:06:40 +0200

    subiendo primer commit de Rover

---
+++ WebApplication1/spec/RoverSpec.js
@@ -0,0 +1,23 @@
+﻿describe("Rover", function () {
+    var rover;
+
+    beforeEach(function () {
+        rover = new Rover();
+        rover.start(0, 0);
+    });
+
+    it("should be able to start & direction the rover", function () {
+        rover.facing('N');        
+        expect(rover.x).toEqual(0);
+        expect(rover.direction).toEqual('N');
+    });
+
+    it("should be able to move the rover to any directions", function () {
+        rover.facing('N');
+        rover.move('ffrfflff');
+        expect(rover.direction).toEqual('N');
+        expect(rover.endUpX).toEqual(2);
+        expect(rover.endUpY).toEqual(4);
+    });
+
+});
---
+++ WebApplication1/src/Rover.js
@@ -0,0 +1,60 @@
+﻿function Rover() {    
+}
+
+Rover.prototype.start = function (x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Rover.prototype.facing = function (direction) {
+    this.direction = direction;
+}
+
+Rover.prototype.move = function (command) {
+    if (command.length > 0) {
+
+        for (var i = 0; i < command.length; i++) {
+            var currentStatus = command[i]; 
+            if (currentStatus == "f" || currentStatus == "b")
+            {
+                switch (this.direction) {
+                    case "N":
+                        if (currentStatus == "f") { this.y++; }
+                        else { this.y--; }
+                        break;
+                    case "S":
+                        if (currentStatus == "f") { this.y--; }
+                        else { this.y++; }
+                        break;
+                    case "W":
+                        if (currentStatus == "f") { this.x--; }
+                        else { this.x++; };
+                        break;
+                    case "E":
+                        if (currentStatus == "f") { this.x++; }
+                        else { this.x--; }
+                        break;
+                }
+                
+            }
+            else if (currentStatus == "r") {
+                switch (this.direction) {
+                    case "N": this.direction = 'E'; break;
+                    case "S": this.direction = 'W'; break;
+                    case "W": this.direction = 'N'; break;
+                    case "E": this.direction = 'S'; break;
+                }
+            }
+            else if (command[i] == "l") {
+                switch (this.direction) {
+                    case "N": this.direction = 'W'; break;
+                    case "S": this.direction = 'E'; break;
+                    case "W": this.direction = 'S'; break;
+                    case "E": this.direction = 'N'; break;
+                }
+            }
+        }
+    }
+    this.endUpX = this.x;
+    this.endUpY = this.y;
+};
